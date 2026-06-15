ba7c409b8ab01c8339eafdaa194cc3b9119b0ca2
Ish200565 <ishikabelel@gmail.com>
2026-06-15 16:14:11 +0530

    jwt expires in 24 hrs

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from dotenv import load_dotenv
 import os
 
@@ -7,4 +9,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
     GROQ_API_KEY=os.getenv("GROQ_API_KEY")
